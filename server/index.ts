4816f3d9a1e9eb40830a4422a0566472c1366e1e
neuenhaus14 <neuenhaus14@gmail.com>
2024-02-01 17:36:23 -0600

    make it better

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,19 @@
 import express, { Request, Response, Router } from "express";
-
+import path from 'path';
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
 app.get("/api", (req: Request, res: Response) => {
   res.send({ message: "Hello from server!" });
+});
+
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
 app.listen(PORT, () => {
