827953c4deb050a995a7814ce17f1ce9192e82f6
neuenhaus14 <neuenhaus14@gmail.com>
2024-02-05 17:31:39 -0600

    updates static serving agagin

--- server/index.ts
+++ server/index.ts
@@ -3,11 +3,12 @@ const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 
+
 const app = express();
 
 // Have Node serve the files for our built React app
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('../client/build'));
+  app.use(express.static(path.join(__dirname, '../client/build')));
 }
 // app.use(express.static(path.resolve(__dirname, '../client/build')));
 
@@ -18,8 +19,10 @@ app.get("/api", (req, res) => {
 
 // All other GET requests not handled before will return our React app
 app.get('/*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'), function (err: Error) {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'), function (err: Error) {
     if (err) {
+      console.log("error:", err)
+      console.log("process.env.PORT", process.env.PORT)
       res.status(500).send(err);
     }
   }
