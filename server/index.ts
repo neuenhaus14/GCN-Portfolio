e38ded1db0609894c68941184fe7ce2b9d805911
neuenhaus14 <neuenhaus14@gmail.com>
2024-02-05 17:39:23 -0600

    more chages to index.ts

--- server/index.ts
+++ server/index.ts
@@ -8,18 +8,10 @@ const app = express();
 
 // Have Node serve the files for our built React app
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
-// app.use(express.static(path.resolve(__dirname, '../client/build')));
-
-// Handle GET requests to /api route
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
-});
-
-// All other GET requests not handled before will return our React app
+  app.use(express.static('client/build'));
+  // All other GET requests not handled before will return our React app
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build', 'index.html'), function (err: Error) {
+  res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'), function (err: Error) {
     if (err) {
       console.log("error:", err)
       console.log("process.env.PORT", process.env.PORT)
@@ -28,7 +20,17 @@ app.get('/*', (req, res) => {
   }
   );
 });
+}
+// app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+// Handle GET requests to /api route
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+
+
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
+  console.log("process.env.PORT", process.env.PORT)
 });
