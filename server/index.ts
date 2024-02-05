ded22c8c53e2fb84e463ae7d2ec70f0dd3376dcb
neuenhaus14 <neuenhaus14@gmail.com>
2024-02-05 17:05:50 -0600

    added dev depences

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response, Router } from "express";
 const path = require('path');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
