c18ab3ce8c4216233280399801f93987a06c232c
neuenhaus14 <neuenhaus14@gmail.com>
2024-02-05 16:09:34 -0600

    Fix dependencies and scripts

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,21 @@
 import express, { Request, Response, Router } from "express";
+const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.get("/api", (req: Request, res: Response) => {
-  res.send({ message: "Hello from server!" });
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+// Handle GET requests to /api route
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
 app.listen(PORT, () => {
