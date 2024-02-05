21c851ca4becc52f41078ff335929ef92302956c
neuenhaus14 <neuenhaus14@gmail.com>
2024-02-05 16:49:16 -0600

    updates package.jsons again w homepage

--- server/index.ts
+++ server/index.ts
@@ -15,7 +15,7 @@ app.get("/api", (req, res) => {
 
 // All other GET requests not handled before will return our React app
 app.get('/*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/build'), function (err: Error) {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'), function (err: Error) {
     if (err) {
       res.status(500).send(err);
     }
