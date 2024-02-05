5896ff4fd36349df28eeca5adb3cbe119dd389ae
neuenhaus14 <neuenhaus14@gmail.com>
2024-02-05 17:50:59 -0600

    previous commit got rid of the duplicate port error due to old start script

--- server/index.ts
+++ server/index.ts
@@ -11,17 +11,16 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
   // All other GET requests not handled before will return our React app
 }
-  app.use('/', express.static(path.join(__dirname, '../client/build', 'index.html')));
-// app.get('/*', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'), function (err: Error) {
-//     if (err) {
-//       console.log("error:", err)
-//       console.log("process.env.PORT", process.env.PORT)
-//       res.status(500).send(err);
-//     }
-//   }
-//   );
-// });
+  //app.use('/', express.static(path.join(__dirname, '../client/build', 'index.html')));
+app.get('/*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'), function (err: Error) {
+    if (err) {
+      console.log("error:", err)
+      res.status(500).send(err);
+    }
+  }
+  );
+});
 
 // app.use(express.static(path.resolve(__dirname, '../client/build')));
 
