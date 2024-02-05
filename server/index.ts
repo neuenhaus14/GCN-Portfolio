a129b4a18d059de3ea41f1192cb26321bdfdb480
neuenhaus14 <neuenhaus14@gmail.com>
2024-02-05 17:53:17 -0600

    workig on app.get now

--- server/index.ts
+++ server/index.ts
@@ -12,15 +12,16 @@ if (process.env.NODE_ENV === 'production') {
   // All other GET requests not handled before will return our React app
 }
   //app.use('/', express.static(path.join(__dirname, '../client/build', 'index.html')));
-app.get('/*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'), function (err: Error) {
-    if (err) {
-      console.log("error:", err)
-      res.status(500).send(err);
-    }
-  }
-  );
-});
+  app.get('/*', (req: Request, res: Response) => {
+    const indexPath = path.join(__dirname, '../client/build', 'index.html');
+    res.sendFile(indexPath, (err: Error) => {
+      if (err) {
+        console.error("Error:", err);
+        res.status(500).send(err);
+      }
+    });
+  });
+  
 
 // app.use(express.static(path.resolve(__dirname, '../client/build')));
 
