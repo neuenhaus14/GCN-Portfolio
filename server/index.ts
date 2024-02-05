9636b770c95a460d1a8e07af9e7883231fa6412a
neuenhaus14 <neuenhaus14@gmail.com>
2024-02-05 17:22:50 -0600

    changed node serving

--- server/index.ts
+++ server/index.ts
@@ -6,7 +6,10 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+// app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 // Handle GET requests to /api route
 app.get("/api", (req, res) => {
