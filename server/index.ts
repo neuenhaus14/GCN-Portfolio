6ebea1abffc677427cae74044941f46d771ae4f2
neuenhaus14 <neuenhaus14@gmail.com>
2024-02-05 18:10:28 -0600

    workig on app.get now

--- server/index.ts
+++ server/index.ts
@@ -6,21 +6,18 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-/// Have Node serve the files for our built React app
-if (process.env.NODE_ENV === 'production') {
-  // Serve the static files from the React app
-  app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 
-  // Handle GET requests to /api route
-  app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
-  });
+// Handle GET requests to /api route
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
 
-  // All other GET requests not handled before will return our React app
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
-  });
-}
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
 
 // app.use(express.static(path.resolve(__dirname, '../client/build')));
 
