f48de214c1f12e44b94f770b8db94a66a82e1d4d
neuenhaus14 <neuenhaus14@gmail.com>
2024-02-05 18:02:51 -0600

    workig on app.get now

--- server/index.ts
+++ server/index.ts
@@ -7,28 +7,30 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Have Node serve the files for our built React app
-if (process.env.NODE_ENV === 'production') {
-  app.use('/', express.static(path.join(__dirname, '../client/build')));
-  // All other GET requests not handled before will return our React app
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(process.cwd(), 'client/build')));
 }
-  //app.use('/', express.static(path.join(__dirname, '../client/build', 'index.html')));
-  app.get('/*', (req: Request, res: Response) => {
-    const indexPath = path.join(__dirname, '../client/build', 'index.html');
-    res.sendFile(indexPath, (err: Error) => {
-      if (err) {
-        console.error("Error:", err);
-        res.status(500).send(err);
-      }
-    });
-  });
-  
-
-// app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 // Handle GET requests to /api route
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
+
+
+// All other GET requests not handled before will return the React app
+app.get('/*', (req: Request, res: Response) => {
+  const indexPath = path.join(process.cwd(), 'client/build', 'index.html');
+  res.sendFile(indexPath, (err: Error) => {
+    if (err) {
+      console.error("Error:", err);
+      res.status(500).send(err);
+    }
+  });
+});
+  
+
+// app.use(express.static(path.resolve(__dirname, '../client/build')));
+
 
 
 
