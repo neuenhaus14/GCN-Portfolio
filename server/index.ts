068e6f9df4539c9e3b05a5127e2832b7ba9415c4
neuenhaus14 <neuenhaus14@gmail.com>
2024-02-15 13:55:17 -0600

    changed index.ts static paths

--- server/index.ts
+++ server/index.ts
@@ -9,9 +9,9 @@ const app = express();
 
 // Have Node serve the files for our built React app
 // this should work
-// const distPath = path.resolve(__dirname, '..', "client");
-// app.use(express.static(distPath));
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+const distPath = path.resolve(__dirname, '..', "client", "build");
+app.use(express.static(distPath));
+// app.use(express.static(path.resolve(__dirname, '../client/build')));
 app.use(express.json())
 
 // Handle GET requests to /api route
@@ -19,10 +19,10 @@ app.get("/api", (req, res) => {
   res.send({ message: "Hello from server!" });
 });
 
-// All other GET requests not handled before will return our React app
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-});
+// // All other GET requests not handled before will return our React app
+// app.get('*', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+// });
 
 // app.use(express.static(path.resolve(__dirname, '../client/build')));
 
