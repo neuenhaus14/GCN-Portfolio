9e2c423d29c54d10bfb3fe0ae1c9b755af546c7c
neuenhaus14 <neuenhaus14@gmail.com>
2024-02-28 16:24:31 -0600

    Build for deployment

--- server/index.ts
+++ server/index.ts
@@ -12,7 +12,7 @@ app.use(express.json())
 
 // Handle GET requests to /api route
 app.get("/api", (req, res) => {
-  res.send({ message: "You're doing great SOSos" });
+  res.send({ message: "You're doing great SOSosos" });
 });
 
 // All other GET requests not handled before will return our React app
