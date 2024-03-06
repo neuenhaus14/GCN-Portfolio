a71b6af2c9ef0a9b19357027bb0c4c6081c98d78
neuenhaus14 <neuenhaus14@gmail.com>
2024-03-06 15:58:01 -0600

    adds PG desc to projects page andsets up formatting for others

--- client/src/Carousel.tsx
+++ client/src/Carousel.tsx
@@ -4,7 +4,6 @@ import { Carousel } from 'react-bootstrap';
 import event from "./img/pgScreenShots/event.png";
 import gigs from "./img/pgScreenShots/gigs.png";
 import home from "./img/pgScreenShots/home.png";
-//import map from ".//img/pgScreenShots/map.png"
 import map from "./img/pgScreenShots/map.png";
 
 const PGCarousel = () => {
@@ -23,18 +22,16 @@ const PGCarousel = () => {
   };
 
   return (
-    <div className="container-sm" style={{display: "flex", justifyContent: "center", alignItems:"center", width: "400px"}}>
+    // <div className="container-sm" style={{display: "flex", justifyContent: "center", flexDirection: "column", flexWrap: "wrap", alignItems:"center", width: "400px"}}>
+    // PARDI GRAS
     <Carousel className="carousel-container"
               activeIndex={index}
               onSelect={handleSelect}
               nextIcon={<span aria-hidden="true" className="carousel-control-next-icon changed" />}
-              style={{display: "flex", justifyContent: "center", alignItems:"center", width: "400px"}}
     >
       {pardiImages.map((slide, index) => (
         <Carousel.Item key={index} style={{}}>
-          
           <p>{slide.title}</p>
-          
           <img
             className="d-block"
             src={slide.image}
@@ -47,51 +44,9 @@ const PGCarousel = () => {
         </Carousel.Item>
       ))}
     </Carousel>
-    </div>
+    // </div>
   );  
 
-  // return (
-  // <Container>
-  //   <Carousel interval={7000} data-bs-theme="dark">
-  //     <Carousel.Item>
-  //       <div className="carousel-item">
-  //         <h5 className="text-center">Stay Connected to the Community</h5>
-  //         <p className="mx-5 mb-3 lh-sm login-text text-center">
-  //           Posts & pics keep event-goers in-the-know about the festivities.
-  //         </p>
-  //         <img src={home} alt="Home" className="carousel-img" width="70%" height="auto" />
-  //       </div>
-  //     </Carousel.Item>
-  //     <Carousel.Item>
-  //       <div className="carousel-item">
-  //         <h5 className="text-center">Find Your Bearings</h5>
-  //         <p className="mx-5 mb-3 lh-sm login-text text-center">
-  //           Map pins mark points of interest and your friends' real-time locations.
-  //         </p>
-  //         <img src={map} alt="Map" className="carousel-img" width="70%" height="auto"/>
-  //       </div>
-  //     </Carousel.Item>
-  //     <Carousel.Item>
-  //       <div className="carousel-item">
-  //         <h5 className="text-center">Get into the Groove</h5>
-  //         <p className="mx-5 mb-3 lh-sm login-text text-center">
-  //           Plan your night out with an up-to-date NOLA live music calendar.
-  //         </p>
-  //         <img src={gigs} alt="Gigs" className="carousel-img" width="70%" height="auto"/>
-  //       </div>
-  //     </Carousel.Item>
-  //     <Carousel.Item>
-  //       <div className="carousel-item">
-  //         <h5 className="text-center">Mark Your Calendar</h5>
-  //         <p className="mx-5 mb-3 lh-sm login-text text-center">
-  //           Create geo-located events and invite your krewe to tag along.
-  //         </p>
-  //         <img src={event} alt="Event" className="carousel-img" width="70%" height="auto"/>
-  //       </div>
-  //     </Carousel.Item>
-  //   </Carousel>
-  // </Container>
-  // )
 }
 
-export default PGCarousel
+export default PGCarousel;
--- client/src/Projects.tsx
+++ client/src/Projects.tsx
@@ -3,15 +3,99 @@ import React from "react";
 import NavBar from "./NavBar";
 import PGCarousel from "./Carousel";
 
-
-
-
 const Projects = () => {
   return (
     <div>
       <NavBar />
-      Pardi Gras
-      <PGCarousel />
+      <div
+        className="container-sm"
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-evenly",
+          flexWrap: "wrap",
+          alignItems: "center",
+        }}
+      >
+        <div className="row">
+          <div className="col-sm">
+            <a href="https://pardigras.org/" style={{fontSize: "24px", fontWeight:600}}>PARDI GRAS</a>
+            <p>Click on the link to see the full site in action or read about it below!</p> 
+            <br/>
+            Your one-stop shop for controlling the chaos of Mardi Gras!  <br/>
+            Pardi Gras crowdsources content from its revelers to map essential <br/> 
+            services like restrooms, emergency personnel, and vendors to keep <br/>
+            everyone in the loop.Share gossip, show off your costume, and brag <br/>
+            about your Zulu Coconut. Create events with your Krewe to plan your <br/>
+            day, peruse through local music gigs, and stay in the know about <br/>
+            upcoming parades.But most importantly: Laissez les bons temps rouler!<br/>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-sm"></div>
+          <PGCarousel />
+        </div>
+      </div>
+
+      <div
+        className="container-sm"
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-evenly",
+          flexWrap: "wrap",
+          alignItems: "center",
+        }}
+      >
+        <div className="row">
+          <div className="col-sm"></div>
+            <PGCarousel />
+        </div>
+        <div className="row">
+          <div className="col-sm">
+            <a href="https://pardigras.org/" style={{fontSize: "24px", fontWeight:600}}>TAKE-A-HIKE</a>
+            <p>Click on the link to see the full site in action or read about it below!</p> 
+            <br/>
+            Your one-stop shop for controlling the chaos of Mardi Gras!  <br/>
+            Pardi Gras crowdsources content from its revelers to map essential <br/> 
+            services like restrooms, emergency personnel, and vendors to keep <br/>
+            everyone in the loop.Share gossip, show off your costume, and brag <br/>
+            about your Zulu Coconut. Create events with your Krewe to plan your <br/>
+            day, peruse through local music gigs, and stay in the know about <br/>
+            upcoming parades.But most importantly: Laissez les bons temps rouler!<br/>
+          </div>
+        </div>
+      </div>
+
+      <div
+        className="container-sm"
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-evenly",
+          flexWrap: "wrap",
+          alignItems: "center",
+        }}
+      >
+        <div className="row">
+          <div className="col-sm">
+            <a href="https://pardigras.org/" style={{fontSize: "24px", fontWeight:600}}>QUIZ PARTY</a>
+            <p>Click on the link to see the full site in action or read about it below!</p> 
+            <br/>
+            Your one-stop shop for controlling the chaos of Mardi Gras!  <br/>
+            Pardi Gras crowdsources content from its revelers to map essential <br/> 
+            services like restrooms, emergency personnel, and vendors to keep <br/>
+            everyone in the loop.Share gossip, show off your costume, and brag <br/>
+            about your Zulu Coconut. Create events with your Krewe to plan your <br/>
+            day, peruse through local music gigs, and stay in the know about <br/>
+            upcoming parades.But most importantly: Laissez les bons temps rouler!<br/>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-sm"></div>
+          <PGCarousel />
+        </div>
+      </div>
     </div>
   );
 };
